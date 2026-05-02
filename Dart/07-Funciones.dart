80fec3574dc08aada21d2e3d20003a5c07e51b25
Edomocar <edumoralescarlos@gmail.com>
2026-05-02 16:49:09 -0500

    Agregar archivo de ejemplo en Dart que demuestra el uso de funciones

---
+++ Dart/07-Funciones.dart
@@ -0,0 +1,19 @@
+String greetEveryone() => 'Hola everyone!';
+
+int sumNumbers(int a, int b) => a + b;
+
+int sumNumbersOptional (int a, int b) {
+  return a + b;
+}
+
+
+void main() {
+
+  print(greetEveryone());
+
+  final int a = 4;
+  final int b = 6;
+  s
+  print("\Suma: $a + $b");
+  print("Resultado: ${sumNumbers(a, b)}");
+}
