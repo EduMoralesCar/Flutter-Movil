4da6c82917338b7b6b77550aaa39aa64d18b8b31
Edomocar <edumoralescarlos@gmail.com>
2026-05-02 11:18:00 -0500

    Actualizar ejemplo en Dart para mostrar el uso de listas, iterables y conjuntos

--- Dart/05-List_Maps_Iterable.dart
+++ Dart/05-List_Maps_Iterable.dart
@@ -1,23 +1,23 @@
 void main() {
+  // Lista, Iterables y Sets
+  final numeros = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9];
+
+  print("Lista Original: ${numeros}");
+  print("Longitud: ${numeros.length}");
+  print("Index 0: ${numeros[0]}");
+  print("Inversa: ${numeros.reversed}");
+
+  final numerosInversos = numeros.reversed;
+  print("\nIterable: ${numerosInversos}");
+  print("Lista: ${numerosInversos.toList()}");
+  print("Set: ${numerosInversos.toSet()}");
+
   
-  final numbers = [1,2,3,4,5,5,5,6,7,8,9,9,10];
-  
-  print('List original $numbers');
-  print('Length ${numbers.length}');
-  print('Index 0: ${numbers[0]}');
-  print('First: ${ numbers.first }');
-  print('Reversed: ${ numbers.reversed }');
-  
-  final reversedNumbers = numbers.reversed;
-  print('Iterable: $reversedNumbers');
-  print('List: ${ reversedNumbers.toList() }');
-  print('Set: ${ reversedNumbers.toSet() }');
-  
-  final numbersGreaterThan5 = numbers.where( (int num) {
-    return num > 5; // true
+  final valorUnico = numeros.toSet();
+  final numMayor5 = valorUnico.where((num) {
+    return num > 5; // True
   });
-  
-  print('>5 iterable: $numbersGreaterThan5');
-  print('>5 Set: ${ numbersGreaterThan5.toSet() }');
-  
+
+  print("\nLista: ${valorUnico}");
+  print("Numero Mayores que 5: ${numMayor5}");
 }
